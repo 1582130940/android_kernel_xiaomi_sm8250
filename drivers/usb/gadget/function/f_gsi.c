4cfc68332018a1ed168c85ee72eb97d73a0e633b
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-03-14 11:09:32 +0800

    drivers/usb: Import Xiaomi changes
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-authored-by: Davide Garberi <dade.garberi@gmail.com>

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -2883,8 +2883,15 @@ static int gsi_bind(struct usb_configuration *c, struct usb_function *f)
 		 * Default to rndis over ethernet which loads NDIS6 drivers
 		 * for windows7/windows10 to avoid data stall issues
 		 */
+#ifdef CONFIG_MACH_XIAOMI
+		if (cdev->isMSOS)
+			gsi->rndis_id = MISC_RNDIS_OVER_ETHERNET;
+		else
+			gsi->rndis_id = WIRELESS_CONTROLLER_REMOTE_NDIS;
+#else
 		if (gsi->rndis_id == RNDIS_ID_UNKNOWN)
 			gsi->rndis_id = MISC_RNDIS_OVER_ETHERNET;
+#endif
 
 		switch (gsi->rndis_id) {
 		default:
