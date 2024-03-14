c541d54de0f4e2775a66e18e9fe755c89062be33
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-03-14 11:59:38 +0800

    fs: Import Xiaomi changes
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/linux/file.h
+++ include/linux/file.h
@@ -83,6 +83,9 @@ extern int f_dupfd(unsigned int from, struct file *file, unsigned flags);
 extern int replace_fd(unsigned fd, struct file *file, unsigned flags);
 extern void set_close_on_exec(unsigned int fd, int flag);
 extern bool get_close_on_exec(unsigned int fd);
+#ifdef CONFIG_MACH_XIAOMI
+extern int get_unused_fd_start_flags(unsigned start, unsigned flags);
+#endif
 extern int get_unused_fd_flags(unsigned flags);
 extern void put_unused_fd(unsigned int fd);
 
