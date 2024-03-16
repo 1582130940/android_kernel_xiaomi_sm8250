0ad53f75bd39a3cc5ca3f6bfbed8f834826b8638
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-03-16 22:11:17 +0100

    ufs: Fix warnings
    Change-Id: Ia1e70e9a7adf3f01f2f37d1e0f7e21db397f9b74

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8477,9 +8477,15 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 {
 	int err;
 	size_t buff_len;
+#ifdef CONFIG_MACH_XIAOMI
+	u8 model_index;
+#else
 	u8 model_index, lun;
+#endif
 	u8 *desc_buf;
+#ifndef CONFIG_MACH_XIAOMI
 	u32 d_lu_wb_buf_alloc;
+#endif
 
 	buff_len = max_t(size_t, hba->desc_size.dev_desc,
 			 QUERY_DESC_MAX_SIZE + 1);
@@ -8548,7 +8554,9 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 	}
 #endif
 
+#ifndef CONFIG_MACH_XIAOMI
 skip_unit_desc:
+#endif
 	/* Zero-pad entire buffer for string termination. */
 	memset(desc_buf, 0, buff_len);
 
