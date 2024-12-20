e90b7c5d6c1d48b98ce33dd4164461887f19339c
Davide Garberi <dade.garberi@gmail.com>
2024-12-20 19:26:14 +0800

    drivers/esoc/esoc-mdm-drv: Honor BOOT_FAIL_ACTION_DEF value
     * This value is supposed to be used to choose what to do when
       the modem restart fails, so let's actually make things behave
       that way.
    
    Change-Id: I4a206694c8c056abf89ec89a42b99d4da4d5aabf

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -71,6 +71,11 @@ int esoc_set_boot_fail_action(struct esoc_clink *esoc_clink, u32 action)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MACH_XIAOMI
+	if (action == BOOT_FAIL_ACTION_PANIC)
+		action = BOOT_FAIL_ACTION_DEF;
+#endif
+
 	if (!mdm_drv) {
 		esoc_mdm_log("esoc-mdm driver not present\n");
 		return -EAGAIN;
@@ -389,7 +394,11 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 	if (*pon_trial == atomic_read(&mdm_drv->n_pon_tries)) {
 		esoc_mdm_log("Reached max. number of boot trials\n");
 		atomic_set(&mdm_drv->boot_fail_action,
+#ifdef CONFIG_MACH_XIAOMI
+					BOOT_FAIL_ACTION_DEF);
+#else
 					BOOT_FAIL_ACTION_PANIC);
+#endif
 	}
 
 	switch (atomic_read(&mdm_drv->boot_fail_action)) {
