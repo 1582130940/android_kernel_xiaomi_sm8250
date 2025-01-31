192e496f8a2308734e5c805b4e5810b65c08a75e
Demon000 <demonsingur@gmail.com>
2025-01-31 19:12:03 +0800

    nt36523: Remove tests
    Change-Id: Ia6e08e87c96d02664d55c51b23f1a615fb65522a

--- drivers/input/touchscreen/nt36523/nt36xxx.c
+++ drivers/input/touchscreen/nt36523/nt36xxx.c
@@ -36,9 +36,6 @@
 #endif
 
 #include "nt36xxx.h"
-#ifndef NVT_SAVE_TESTDATA_IN_FILE
-#include "nt36xxx_mp_ctrlram.h"
-#endif
 #if NVT_TOUCH_ESD_PROTECT
 #include <linux/jiffies.h>
 #endif /* #if NVT_TOUCH_ESD_PROTECT */
@@ -61,11 +58,6 @@ pen_charge_state_notifier_unregister_client(struct notifier_block *nb);
 #if NVT_TOUCH_EXT_PROC
 extern int32_t nvt_extra_proc_init(void);
 extern void nvt_extra_proc_deinit(void);
-#endif
-
-#if NVT_TOUCH_MP
-extern int32_t nvt_mp_proc_init(void);
-extern void nvt_mp_proc_deinit(void);
 #endif
 
 static int32_t nvt_ts_suspend(struct device *dev);
@@ -2986,22 +2978,6 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	}
 #endif
 
-#if NVT_TOUCH_MP
-	ret = nvt_mp_proc_init();
-	if (ret != 0) {
-		NVT_ERR("nvt mp proc init failed. ret=%d\n", ret);
-		goto err_mp_proc_init_failed;
-	}
-#endif
-
-#ifndef NVT_SAVE_TESTDATA_IN_FILE
-	ret = nvt_test_data_proc_init(ts->client);
-	if (ret < 0) {
-		NVT_ERR("nvt test data interface init failed. ret=%d\n", ret);
-		goto err_mp_proc_init_failed;
-	}
-#endif
-
 	ts->event_wq =
 		alloc_workqueue("nvt-event-queue",
 				WQ_UNBOUND | WQ_HIGHPRI | WQ_CPU_INTENSIVE, 1);
@@ -3096,13 +3072,6 @@ err_create_set_touchfeature_work_queue:
 	destroy_workqueue(ts->event_wq);
 err_alloc_work_thread_failed:
 
-#ifndef NVT_SAVE_TESTDATA_IN_FILE
-	nvt_test_data_proc_deinit();
-#endif
-#if NVT_TOUCH_MP
-	nvt_mp_proc_deinit();
-err_mp_proc_init_failed:
-#endif
 #if NVT_TOUCH_EXT_PROC
 	nvt_extra_proc_deinit();
 err_extra_proc_init_failed:
@@ -3210,9 +3179,6 @@ static int32_t nvt_ts_remove(struct spi_device *client)
 	if (pen_charge_state_notifier_unregister_client(
 		    &ts->pen_charge_state_notifier))
 		NVT_ERR("Error occurred while unregistering pen charge state notifier.\n");
-#if NVT_TOUCH_MP
-	nvt_mp_proc_deinit();
-#endif
 #if NVT_TOUCH_EXT_PROC
 	nvt_extra_proc_deinit();
 #endif
@@ -3303,9 +3269,6 @@ static void nvt_ts_shutdown(struct spi_device *client)
 	if (pen_charge_state_notifier_unregister_client(
 		    &ts->pen_charge_state_notifier))
 		NVT_ERR("Error occurred while unregistering pen charge state notifier.\n");
-#if NVT_TOUCH_MP
-	nvt_mp_proc_deinit();
-#endif
 #if NVT_TOUCH_EXT_PROC
 	nvt_extra_proc_deinit();
 #endif
