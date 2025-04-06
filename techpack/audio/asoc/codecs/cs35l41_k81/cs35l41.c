1c2e7228815ce99b3907cca7cc21cd0e42dd4b98
David Rhodes <david.rhodes@cirrus.com>
2025-04-06 18:56:07 +0800

    cs35l41_k81/cs35l41: Namespace structs
    Namespace the IRQ config and Class H config structs
    
    Change-Id: I2ae807ac52f5e3ab5e76d2ec5a743594820cd2b8
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- techpack/audio/asoc/codecs/cs35l41_k81/cs35l41.c
+++ techpack/audio/asoc/codecs/cs35l41_k81/cs35l41.c
@@ -2504,7 +2504,7 @@ static int cs35l41_boost_config(struct cs35l41_private *cs35l41, int boost_ind,
 
 static int cs35l41_set_pdata(struct cs35l41_private *cs35l41)
 {
-	struct classh_cfg *classh = &cs35l41->pdata.classh_config;
+	struct cs35l41_classh_cfg *classh = &cs35l41->pdata.classh_config;
 	int ret;
 	u32 devid_otp;
 
@@ -2724,8 +2724,8 @@ exit:
 
 static int cs35l41_irq_gpio_config(struct cs35l41_private *cs35l41)
 {
-	struct irq_cfg *irq_gpio_cfg1 = &cs35l41->pdata.irq_config1;
-	struct irq_cfg *irq_gpio_cfg2 = &cs35l41->pdata.irq_config2;
+	struct cs35l41_irq_cfg *irq_gpio_cfg1 = &cs35l41->pdata.irq_config1;
+	struct cs35l41_irq_cfg *irq_gpio_cfg2 = &cs35l41->pdata.irq_config2;
 	int irq_pol = IRQF_TRIGGER_NONE;
 
 	if (irq_gpio_cfg1->is_present) {
@@ -2827,9 +2827,9 @@ static int cs35l41_handle_of_data(struct device *dev,
 	int ret;
 	size_t num_fast_switch;
 	struct device_node *sub_node;
-	struct classh_cfg *classh_config = &pdata->classh_config;
-	struct irq_cfg *irq_gpio1_config = &pdata->irq_config1;
-	struct irq_cfg *irq_gpio2_config = &pdata->irq_config2;
+	struct cs35l41_classh_cfg *classh_config = &pdata->classh_config;
+	struct cs35l41_irq_cfg *irq_gpio1_config = &pdata->irq_config1;
+	struct cs35l41_irq_cfg *irq_gpio2_config = &pdata->irq_config2;
 	unsigned int i;
 
 	if (!np)
