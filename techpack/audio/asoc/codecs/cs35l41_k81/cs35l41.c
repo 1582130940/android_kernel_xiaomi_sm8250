5bc25838e8a20a0d3b6214f8c441fd987aed109c
Charles Keepax <ckeepax@opensource.cirrus.com>
2025-04-06 19:33:46 +0800

    cs35l41_k81/cs35l41: Fix use of an uninitialised variable
    The loop checking PDN_DONE doesn't check the return value from
    regmap_read, nor does it initialise val. This means if regmap_read fails
    val will be checked for the PDN_DONE bit whilst being uninitialised.
    
    Fix this up by switching to regmap_read_poll_timeout which tidies up the
    code and avoids the uninitialised variable.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20210914141349.30218-1-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- techpack/audio/asoc/codecs/cs35l41_k81/cs35l41.c
+++ techpack/audio/asoc/codecs/cs35l41_k81/cs35l41.c
@@ -1821,7 +1821,6 @@ static int cs35l41_main_amp_event(struct snd_soc_dapm_widget *w,
 	struct cs35l41_private *cs35l41 = snd_soc_component_get_drvdata(component);
 	enum cs35l41_cspl_mboxcmd mboxcmd = CSPL_MBOX_CMD_NONE;
 	int ret = 0;
-	int i;
 	bool pdn;
 	unsigned int val;
 
@@ -1865,18 +1864,11 @@ static int cs35l41_main_amp_event(struct snd_soc_dapm_widget *w,
 				   CS35L41_GLOBAL_EN_MASK, 0);
 
 		pdn = false;
-		for (i = 0; i < 100; i++) {
-			regmap_read(cs35l41->regmap, CS35L41_IRQ1_STATUS1,
-				    &val);
-			if (val & CS35L41_PDN_DONE_MASK) {
-				pdn = true;
-				break;
-			}
-			usleep_range(1000, 1010);
-		}
-
-		if (!pdn)
-			dev_warn(cs35l41->dev, "PDN failed\n");
+		ret = regmap_read_poll_timeout(cs35l41->regmap, CS35L41_IRQ1_STATUS1,
+					       val, val &  CS35L41_PDN_DONE_MASK,
+					       1000, 100000);
+		if (ret)
+			dev_warn(cs35l41->dev, "PDN failed: %d\n", ret);
 
 		regmap_write(cs35l41->regmap, CS35L41_IRQ1_STATUS1,
 			     CS35L41_PDN_DONE_MASK);
