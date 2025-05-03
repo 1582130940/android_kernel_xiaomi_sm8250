a635981c09585a228adf20eef939217f974db5c4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-05-04 05:11:28 +0800

    fpc: Omit unnecessary GOTO usage to return codes
    This is too convoluted for a simple return check.
    Just return directly instead of using the rc and goto.
    
    This commit brings no functional changes.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/fpc/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_tee.c
@@ -908,8 +908,7 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 	if (!fpc1020) {
 		dev_err(dev,
 			"failed to allocate memory for struct fpc1020_data\n");
-		rc = -ENOMEM;
-		goto exit;
+		return -ENOMEM;
 	}
 
 	fpc1020->dev = dev;
@@ -917,8 +916,7 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 
 	if (!np) {
 		dev_err(dev, "no of node found\n");
-		rc = -EINVAL;
-		goto exit;
+		return -EINVAL;
 	}
 
 	fpc1020->fingerprint_pinctrl = devm_pinctrl_get(dev);
@@ -930,8 +928,7 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 		}
 		dev_err(dev, "Target does not use pinctrl\n");
 		fpc1020->fingerprint_pinctrl = NULL;
-		rc = -EINVAL;
-		goto exit;
+		return -EINVAL;
 	}
 
 	for (i = 0; i < ARRAY_SIZE(pctl_names); i++) {
@@ -940,8 +937,7 @@ static inline int fpc1020_probe(struct platform_device *pdev)
 			pinctrl_lookup_state(fpc1020->fingerprint_pinctrl, n);
 		if (IS_ERR(state)) {
 			dev_err(dev, "cannot find '%s'\n", n);
-			rc = -EINVAL;
-			goto exit;
+			return -EINVAL;
 		}
 		dev_info(dev, "found pin control %s\n", n);
 		fpc1020->pinctrl_state[i] = state;
