83dd88a389184a0e13aaef6d09499471a733426f
Ping Li <pingli@codeaurora.org>
2025-05-28 12:55:01 +0800

    display/msm/sde: Fix potential race condition
    Move the hist irq handling out of callback function, i.e., the hw
    interrupt irq_lock context, to avoid dead lock between crtc spin_lock
    and irq_lock. This change also extends crtc spin_lock coverage in
    _sde_cp_crtc_enable_hist_irq to prevent null pointer dereference on
    event node, which can be deleted during crtc event de-registration.
    
    Change-Id: Iadaed54ab93c4c4abe065a8762d2addccb0c65c6
    Signed-off-by: Ping Li <pingli@codeaurora.org>

--- techpack/display/msm/sde/sde_crtc.h
+++ techpack/display/msm/sde/sde_crtc.h
@@ -286,6 +286,7 @@ struct sde_crtc_misr_info {
  * @ltm_buffer_lock : muttx to protect ltm_buffers allcation and free
  * @ltm_lock        : Spinlock to protect ltm buffer_cnt, hist_en and ltm lists
  * @needs_hw_reset  : Initiate a hw ctl reset
+ * @hist_irq_idx    : hist interrupt irq idx
  * @comp_ratio      : Compression ratio
  * @dspp_blob_info  : blob containing dspp hw capability information
  */
@@ -373,6 +374,7 @@ struct sde_crtc {
 	struct mutex ltm_buffer_lock;
 	spinlock_t ltm_lock;
 	bool needs_hw_reset;
+	int hist_irq_idx;
 
 	int comp_ratio;
 
