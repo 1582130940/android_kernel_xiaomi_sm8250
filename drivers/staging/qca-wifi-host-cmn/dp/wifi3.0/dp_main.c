03301de937a2798dbe3d6f6c1a58c069e9789753
Venkateswara Naralasetty <quic_vnaralas@quicinc.com>
2025-08-30 18:19:01 +0800

    qcacmn: Fix out-of-bound issue in scattered list setup
    Fix out-of-bound issue in scattered list setup.
    
    Change-Id: I8a0c419227de3613d70229e1b6364d7e46dfbf7b
    CRs-Fixed: 3664082

--- drivers/staging/qca-wifi-host-cmn/dp/wifi3.0/dp_main.c
+++ drivers/staging/qca-wifi-host-cmn/dp/wifi3.0/dp_main.c
@@ -2580,8 +2580,11 @@ static int dp_hw_link_desc_pool_setup(struct dp_soc *soc)
 			} else {
 				rem_entries = num_entries_per_buf;
 				scatter_buf_num++;
-				if (scatter_buf_num >= num_scatter_bufs)
+				if (scatter_buf_num >= num_scatter_bufs) {
+					scatter_buf_num--;
 					break;
+				}
+
 				scatter_buf_ptr =
 					(uint8_t *)
 				(soc->wbm_idle_scatter_buf_base_vaddr[
@@ -2596,7 +2599,7 @@ static int dp_hw_link_desc_pool_setup(struct dp_soc *soc)
 			num_scatter_bufs, soc->wbm_idle_scatter_buf_size,
 			(uint32_t)(scatter_buf_ptr -
 			(uint8_t *)(soc->wbm_idle_scatter_buf_base_vaddr[
-			scatter_buf_num-1])), total_link_descs);
+			scatter_buf_num])), total_link_descs);
 	}
 	return 0;
 
