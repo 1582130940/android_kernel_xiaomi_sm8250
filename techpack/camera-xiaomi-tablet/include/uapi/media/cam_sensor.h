1c8b6cc3b8764f81b7ffb3d44b0132b74a85826b
Rocky7842 <eric.rocky7842@gmail.com>
2025-09-09 17:36:56 +0800

    camera-xiaomi-tablet: Implement OIS post_init functions
    * Kanged from xiaomi sm8350. Required by EIS.
    
    Change-Id: I899afe61f34314e8d6215da999cd57a0e169be9e

--- techpack/camera-xiaomi-tablet/include/uapi/media/cam_sensor.h
+++ techpack/camera-xiaomi-tablet/include/uapi/media/cam_sensor.h
@@ -135,6 +135,7 @@ struct cam_ois_opcode {
  * @ois_fw_flag           :    indicates if fw is present or not
  * @is_ois_calib          :    indicates the calibration data is available
  * @is_ois_pre_init       :    indicates the pre initialize data is available
+ * @is_ois_post_init       :    indicates the post initialize data is available
  * @ois_name              :    OIS name
  * @opcode                :    opcode
  */
@@ -145,6 +146,7 @@ struct cam_cmd_ois_info {
 	uint8_t               ois_fw_flag;
 	uint8_t               is_ois_calib;
 	uint8_t               is_ois_pre_init;
+	uint8_t               is_ois_post_init;
 	char                  ois_name[MAX_OIS_NAME_SIZE];
 	struct cam_ois_opcode opcode;
 } __attribute__((packed));
