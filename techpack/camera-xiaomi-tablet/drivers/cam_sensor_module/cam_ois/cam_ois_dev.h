5f379e7352e80335aea283e8a68236a31ad7c942
Rocky7842 <eric.rocky7842@gmail.com>
2025-09-09 17:36:56 +0800

    camera-xiaomi-tablet: Implement OIS post_init functions
    * Kanged from xiaomi sm8350. Required by EIS.
    
    Change-Id: I899afe61f34314e8d6215da999cd57a0e169be9e

--- techpack/camera-xiaomi-tablet/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
+++ techpack/camera-xiaomi-tablet/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
@@ -111,6 +111,8 @@ struct ois_data_eis_t {
  * @device_name     :   Device name
  * @i2c_pre_init_data:  ois i2c pre init settings
  * @is_ois_pre_init :   flag for pre init settings
+ * @i2c_post_init_data:  ois i2c post init settings
+ * @is_ois_post_init :   flag for post init settings
  *
  */
 struct cam_ois_ctrl_t {
@@ -133,7 +135,9 @@ struct cam_ois_ctrl_t {
 	uint8_t is_ois_calib;
 	struct cam_ois_opcode opcode;
 	struct i2c_settings_array i2c_pre_init_data;
+	struct i2c_settings_array i2c_post_init_data;
 	uint8_t is_ois_pre_init;
+	uint8_t is_ois_post_init;
 #ifdef ENABLE_OIS_EIS
 	struct ois_data_eis_t ois_data;
 #endif
