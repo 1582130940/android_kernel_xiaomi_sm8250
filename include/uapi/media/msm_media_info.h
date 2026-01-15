523aee1929081e2b8724eeadb415f22a4ccbf15d
Vasantha Balla <vballa@codeaurora.org>
2026-01-15 13:13:11 +0800

    uapi/media/msm_media_info: Increase extra size for ubwc
    Increase extra UBWC size from 4k to 16k to pass
    thumbnail cts cases for HDR10 content.
    
    Change-Id: I0a0445b5fa421f6fd827d50b06f694c486e4cd33
    Signed-off-by: Vasantha Balla <vballa@codeaurora.org>

--- include/uapi/media/msm_media_info.h
+++ include/uapi/media/msm_media_info.h
@@ -18,7 +18,7 @@
 #define NV12_STRIDE_ALIGNMENT 512
 #define NV12_SCANLINE_ALIGNMENT 512
 #endif
-#define UBWC_EXTRA_SIZE 4096
+#define UBWC_EXTRA_SIZE 16384 /* 16*1024 extra size */
 
 /* Width and Height should be multiple of 16 */
 #define INTERLACE_WIDTH_MAX 1920
