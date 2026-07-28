36f06d9b43ea1ecff7ec02ad4e69138c31891706
kimocoder <christian@aircrack-ng.org>
2026-07-28 17:43:59 +0800

    qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211: Add monitor mode support to netlink

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -17043,6 +17043,7 @@ static bool hdd_is_client_mode(enum QDF_OPMODE mode)
 	case QDF_P2P_CLIENT_MODE:
 	case QDF_P2P_DEVICE_MODE:
 	case QDF_IBSS_MODE:
+	case QDF_MONITOR_MODE:
 		return true;
 	default:
 		return false;
@@ -17054,6 +17055,7 @@ static bool hdd_is_ap_mode(enum QDF_OPMODE mode)
 	switch (mode) {
 	case QDF_SAP_MODE:
 	case QDF_P2P_GO_MODE:
+	case QDF_MONITOR_MODE:
 		return true;
 	default:
 		return false;
